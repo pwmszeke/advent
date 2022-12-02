15421e4d052ea43673d5991c8483c90edacc0c5d
Zeke Pendexter <zpendexter@gmail.com>
2022-12-02 08:14:46 -0500

    Ugly day 2

--- app.py
+++ app.py
@@ -1,18 +1,49 @@
 
-with open('input.txt', 'r') as f:
-    data = f.read()
-max_total = 0
-elf_cals = []
+def main():
+    with open('input.txt', 'r') as f:
+        data = f.readlines()
 
-for elf in data.split("\n\n"):
     total = 0
-    new_elf = elf.split("\n")
-    for item in new_elf:
-        total += int(item)
-    if total > max_total:
-        max_total = total
-    elf_cals.append(total)
 
-top_elfs = sum(sorted(elf_cals)[-3:])
-print(top_elfs)
+    for game in data:
+        inputs = game.strip().split(' ')
+        total += calc_score(inputs[0], inputs[1])
+    print(total)
 
+# A = Rock
+# B = Paper
+# C = Scissors
+
+# X = Lose = 0
+# Y = Draw = 3
+# Z = Win = 6
+
+# Rock  = 1
+# Paper  = 2
+# Scissors   = 3
+def calc_score(them, you):
+    if them == 'A':
+        if you == 'X':
+            return 3
+        if you == 'Y':
+            return 4
+        if you == 'Z':
+            return 8
+    if them == 'B':
+        if you == 'X':
+            return 1
+        if you == 'Y':
+            return 5
+        if you == 'Z':
+            return 9
+    if them == 'C':
+        if you == 'X':
+            return 2
+        if you == 'Y':
+            return 6
+        if you == 'Z':
+            return 7
+
+
+if __name__ == '__main__':
+    main()
