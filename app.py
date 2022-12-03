f050ab55b84eaf0226b39c2c63d801aa6cbd645e
Zeke Pendexter <zpendexter@gmail.com>
2022-12-03 15:44:38 -0500

    Not proud of this one...

--- app.py
+++ app.py
@@ -4,46 +4,24 @@ def main():
         data = f.readlines()
 
     total = 0
+    idx = 0
 
-    for game in data:
-        inputs = game.strip().split(' ')
-        total += calc_score(inputs[0], inputs[1])
+    for line in data:
+        if idx+2 > len(data):
+            break
+        first, second, third = data[idx].strip(), data[idx+1].strip(), data[idx+2].strip()
+        common_letter = ''.join(set(first).intersection(second).intersection(third))
+        print(common_letter)
+        total += get_value(common_letter)
+        idx += 3
+
     print(total)
 
-# A = Rock
-# B = Paper
-# C = Scissors
-
-# X = Lose = 0
-# Y = Draw = 3
-# Z = Win = 6
-
-# Rock  = 1
-# Paper  = 2
-# Scissors   = 3
-def calc_score(them, you):
-    if them == 'A':
-        if you == 'X':
-            return 3
-        if you == 'Y':
-            return 4
-        if you == 'Z':
-            return 8
-    if them == 'B':
-        if you == 'X':
-            return 1
-        if you == 'Y':
-            return 5
-        if you == 'Z':
-            return 9
-    if them == 'C':
-        if you == 'X':
-            return 2
-        if you == 'Y':
-            return 6
-        if you == 'Z':
-            return 7
-
+def get_value(letter):
+    if letter.islower():
+        return ord(letter) - 96    
+    else:
+        return ord(letter) - 38
 
 if __name__ == '__main__':
     main()
