2f0ec72b901cf44d708bb0a123be266cbe16dcfd
zpendexter <zpendexter@gmail.com>
2022-12-09 07:22:08 -0500

    Making repo yearly

---
+++ 2022/8.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+from itertools import accumulate
+
+
+def main():
+    with open('input.txt', 'r') as f:
+        data = f.read().splitlines()
+    
+    total_visible = 0
+    total_trees = 0
+    higher = 0
+    above = False
+    below = False
+    right = False
+    left = False
+
+
+if __name__ == '__main__':
+    main()
