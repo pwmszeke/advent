26cad9b69ecf82287447ab6ffe3693e11831c066
zpendexter <zpendexter@gmail.com>
2022-12-10 11:37:34 -0500

    9.2

--- 2022/9.py
+++ 2022/9.py
@@ -6,34 +6,46 @@ def main():
     with open('inputs/9.txt', 'r') as f:
         data = f.read().splitlines()
     
-    cycle = 1
+    cycle = 0
     x_register = 1
-    signal_sum = 0
+    current_position = 0
+    row = 0
+    crt = [[] for i in range(7)]
 
     for instruction in data:
         if instruction.startswith('no'):
+            draw_pixel(current_position, x_register, crt, row)
             cycle += 1
-            if check_cycle(cycle):
-                print(f'Cycle {cycle}: {x_register}')
-                signal_sum += (cycle * x_register)
+            current_position += 1
+            if cycle % 40 == 0:
+                row = cycle / 40
+                row = int(row)
+                current_position = 0
         else:
             number = int(instruction.split(' ')[1])
+            draw_pixel(current_position, x_register, crt, row)  
             cycle += 1
-            if check_cycle(cycle):
-                print(f'Cycle {cycle}: {x_register}')
-                signal_sum += (cycle * x_register)
+            current_position += 1
+            if cycle % 40 == 0:
+                row = cycle / 40
+                row = int(row)
+                current_position = 0
+            draw_pixel(current_position, x_register, crt, row)             
             cycle += 1
+            current_position += 1
+            if cycle % 40 == 0:
+                row = cycle / 40
+                row = int(row)
+                current_position = 0
             x_register += number
-            if check_cycle(cycle):
-                print(f'Cycle {cycle}: {x_register}')
-                signal_sum += (cycle * x_register)
-    print(signal_sum)
-
-def check_cycle(cycle):
-    values = [20, 60, 100, 140, 180, 220]
-    if cycle in values:
-        return True
-
+    for row in crt:
+        print("".join(str(x) for x in row))
+    
+def draw_pixel(current_position, x_register, crt, row):
+    if current_position in range(x_register-1, x_register+2):
+        crt[row].append('#')
+    else:
+        crt[row].append('.')
 
 if __name__ == '__main__':
     main()
